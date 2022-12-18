3e7dd9ecee47021f7663852bd8275c9715290985
marko-smiljanic <75691933+marko-smiljanic@users.noreply.github.com>
2022-12-18 15:36:49 +0100

    dodati pocetni kod za login

---
+++ Aplikacija-kod/server/api.py
@@ -0,0 +1,111 @@
+#import email
+#from types import MethodDescriptorType
+import flask, json
+from flask import Flask, request
+import json
+
+app = Flask('__main__', template_folder="", static_folder="", root_path="", static_url_path="")
+msgs = []
+
+
+
+@app.route('/')
+def index_page():
+    return ("Hello")
+
+@app.route('/dobaviJednogKorisnika/<string:korisnickoIme>', methods=["GET"])       #moram obezbediti da korisnicko ime bude jedinstveno!!!!
+def prikaz_jednog_korisnika(korisnickoIme):
+    try:
+        with open("korisnici.json") as f:
+            data = json.load(f)
+            for kk in data:
+                if kk["korisnicko_ime"] == korisnickoIme:
+                    return str(kk)
+    except(Exception):
+        return "greska"
+
+@app.route('/dobaviSveKorisnike', methods=["GET"])
+def prikaz_svih():
+    return flask.send_file("korisnici.json")
+
+@app.route('/dodajJednogKorisnika', methods=["POST"])
+def dodaj_korisnika():
+    recnik = dict(flask.request.form)                            #ovo je za za volley iz klijenta?
+    #recnik = dict(flask.request.json)                           #za testiranje u postman-u
+    #print(recnik)
+
+    recnik2 = {                                                 #ovde sam morao da pravim novi recnik i da prepisujem vrednosti iz recnika response-a jer su vrednosti iz nekog razloga obrutim redosledom u njemu
+        "korisnicko_ime": recnik["korisnicko_ime"],
+        "lozinka": recnik["lozinka"],
+        "email": recnik["email"],
+        "platio_ful_verziju": recnik["platio_ful_verziju"]
+    }    
+
+    #treba proveriti i za mejl adresu isto da se ne poklapa sa nekom postojecom, moramo obezbediti da ova dva budu unique
+    with open("korisnici.json", "r") as ff:                     #otvorimo json fajl
+        json_data = json.load(ff)                               #proverimo da ne postoji vec takvo korisnicko ime, jer je cilj da korisnicko ime bude jedinstveno
+        for kk in json_data:
+            if kk["korisnicko_ime"] == recnik2["korisnicko_ime"]:
+                return "korisnicko ime vec postoji"
+            if kk["email"] == recnik2["email"]:
+                return "email adresa vec postoji"
+
+
+        json_data = list(json_data)                                 #pretvorimo u listu jer je tako upisan u json-u, tj. pocinje i zavrsava se sa [ ]
+        json_data.append(recnik2)                                   #dodamo recnik iz response-a
+
+        with open ("korisnici.json", "w") as zz:                    #prepisemo izmenjeni fajl na postojeci, tj. novi samo sacuvamo preko starog (jer tako se menja fajl, obrisi sve sto je bilo i dodaj novi fajl sa izmenama)
+            json.dump(json_data, zz, indent=2)                      #ovde bi mogao dodati indent da mi json fajl zapise u preglednijoj strukturi a ne sve u jednoj liniji
+
+        return "sve ok", 201                                        #promenio sam da response ne bude ""
+
+
+@app.route('/ulogujKorisnika', methods=["POST"])                   #logovanje korisnika sa vec postojecim nalogom
+def uloguj_korisnika():
+    recnik = dict(flask.request.form)                         
+    #recnik = dict(flask.request.json)                           
+    #print(recnik)
+
+    recnik2 = {                                                
+        "korisnicko_ime": recnik["korisnicko_ime"],
+        "lozinka": recnik["lozinka"]
+    }    
+
+    with open("korisnici.json", "r") as ff:                    
+        json_data = json.load(ff)                              
+        for kk in json_data:
+            if kk["korisnicko_ime"] == recnik2["korisnicko_ime"]: 
+                if kk["lozinka"] == recnik2["lozinka"]:
+                    return "sve ok"                                 #ili da vratim korisnicko ime?
+
+        return "neispravan unos" 
+
+
+@app.route('/platiFulVerziju/<string:korisnickoIme>', methods=["GET"])       #moram obezbediti da korisnicko ime bude jedinstveno!!!! to je odradjeno u kreiranju korisnika
+def plati_ful_verziju(korisnickoIme):
+    try:
+        with open("korisnici.json") as f:
+            data = json.load(f)
+            for kk in data:
+                if kk["korisnicko_ime"] == korisnickoIme:
+                    if kk["platio_ful_verziju"] == "true":
+                        return "korisnik je vec platio"
+                    elif kk["platio_ful_verziju"] == "false":
+                        kk["platio_ful_verziju"] = "true"
+
+                        #sada izmenu treba da zapisemo u fajl
+                        data = list(data)                                 #pretvorimo u listu jer je tako upisan u json-u, tj. pocinje i zavrsava se sa [ ]
+
+                        with open ("korisnici.json", "w") as zz:                    #prepisemo izmenjeni fajl na postojeci, tj. novi samo sacuvamo preko starog (jer tako se menja fajl, obrisi sve sto je bilo i dodaj novi fajl sa izmenama)
+                            json.dump(data, zz, indent=2)
+                            return "sve ok"  
+    except(Exception):
+        return "greska"
+                    
+
+
+
+
+#########
+app.run("0.0.0.0", 5000)
+
